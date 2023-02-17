b9952bdfe86b014fb65933e73ecd2a7428b6968f
Chaerul Umam <chaerulumamb244@gmail.com>
2023-02-18 00:58:10 +0700

    first commit and setup navbar

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Navbar } from "./components/organisms";
+
+export default function App() {
+  return (
+    <div className="font-Poppins bg-Solitude">
+      <Navbar />
+    </div>
+  );
+}
---
+++ src/components/molecules/index.jsx
@@ -0,0 +1,4 @@
+import NavLinkItem from "./NavLinkItem";
+import NavLinkMobileItem from "./NavLinkMobileItem.jsx";
+
+export { NavLinkItem, NavLinkMobileItem };

fa3a5d0aec0b5dc5f2ec213107bcd80f1d4a0675
Chaerul Umam <chaerulumamb244@gmail.com>
2023-02-18 09:48:43 +0700

    Home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Navbar } from "./components/organisms";
+import { Home } from "./sections";
 
 export default function App() {
   return (
     <div className="font-Poppins bg-Solitude">
       <Navbar />
+      <Home />
     </div>
   );
 }

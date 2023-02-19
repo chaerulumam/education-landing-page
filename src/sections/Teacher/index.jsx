b54bb3e9014db798a01ab35da0dbae368191d116
Chaerul Umam <chaerulumamb244@gmail.com>
2023-02-19 13:24:11 +0700

    modify teacher item again

--- src/sections/Teacher/index.jsx
+++ src/sections/Teacher/index.jsx
@@ -11,38 +11,38 @@ const Teacher = () => {
         <div className="pl-5">
           <div className="font-bold sm:text-[1.875rem] text-[1.5rem] mb-5">
             Become <span className="text-Teal">An Instructor</span> <br /> of
-            Our Platform
+            Our Platfoem
           </div>
           <p className="text-sm leading-7 text-gray mb-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-            perferendis debitis dolorum facilis culpa, quidem voluptate suscipit
-            deserunt. Magni neque at eos dolore dignissimos fugit repudiandae?
-            Aut laudantium asperiores et!
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            quis vero impedit. Consequatur, saepe. Praesentium doloremque sunt
+            laborum. Fuga a optio quae id culpa labore, veniam vitae similique
+            voluptate, aliquam, quod esse.
           </p>
-          <button className="py-3 px-4 bg-Teal text-white rounded-lg text-sm font-bold ">
+          <button className="py-3 px-4 bg-Teal hover:bg-teal-800 text-white rounded-lg text-sm font-bold">
             Start Teaching
           </button>
         </div>
         <div className="p-4 md:w-3/4 sm:row-start-1">
-          <img src={teacher1} alt="" />
+          <img src={teacher1} alt="Teacher Img" />
         </div>
         <div className="pl-5">
           <div className="font-bold sm:text-[1.875rem] text-[1.5rem] mb-5">
             Become <span className="text-Teal">An Instructor</span> <br /> of
-            Our Platform
+            Our Platfoem
           </div>
           <p className="text-sm leading-7 text-gray mb-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-            perferendis debitis dolorum facilis culpa, quidem voluptate suscipit
-            deserunt. Magni neque at eos dolore dignissimos fugit repudiandae?
-            Aut laudantium asperiores et!
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            quis vero impedit. Consequatur, saepe. Praesentium doloremque sunt
+            laborum. Fuga a optio quae id culpa labore, veniam vitae similique
+            voluptate, aliquam, quod esse.
           </p>
-          <button className="py-3 px-4 bg-Teal text-white rounded-lg text-sm font-bold ">
+          <button className="py-3 px-4 bg-Teal hover:bg-teal-800 text-white rounded-lg text-sm font-bold">
             Get Started
           </button>
         </div>
         <div className="p-4 md:w-3/4">
-          <img src={teacher2} alt="" />
+          <img src={teacher2} alt="Teacher Imagw" />
         </div>
       </div>
       <div className="text-center my-8 font-bold sm:text-[1.875rem] text-[1.5rem]">
